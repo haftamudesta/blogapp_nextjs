4046a18d5d8c56b30465cc84b91503487ef26bba
Haftamu Desta <haftamudesta>
2025-12-15 08:33:48 -0800

    Add betterAuth with convex

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
+import { ConvexClientProvider } from "@/components/web/ConvexClientProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,7 +35,9 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             <main className="max-w-7xl mx-auto w-full px-4 md:px-6 lg:px-6">
-              {children}
+              <ConvexClientProvider>
+                {children}
+              </ConvexClientProvider>
             </main>
           </ThemeProvider>
         
