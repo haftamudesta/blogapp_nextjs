68439b837a0bed96e8b18408c08069eb56b1d637
Haftamu Desta <haftamudesta>
2025-12-18 22:10:30 -0800

    Add loading skeleten

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -1,4 +1,3 @@
-"use client"
 
 import { buttonVariants } from "@/components/ui/button"
 import { Card, CardContent, CardFooter } from "@/components/ui/card"
@@ -6,9 +5,12 @@ import { api } from "@/convex/_generated/api"
 import { useQuery } from "convex/react"
 import Image from "next/image"
 import Link from "next/link"
+import { fetchQuery } from "convex/nextjs";
+import { Suspense } from "react"
+import { Skeleton } from "@/components/ui/skeleton"
 
 export default function blogPost(){
-    const data=useQuery(api.posts.getPost)
+    
     return(
        <main className="py-12">
         <div className="text-center mb-12">
@@ -19,7 +21,18 @@ export default function blogPost(){
                 Latest insights and updates from our team
             </p>
         </div>
-        <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+        <Suspense fallback={<SkeletonLoadingUi/>}>
+            <FetchBlogLists/>
+        </Suspense>
+       </main>
+      
+    )
+}
+
+async function FetchBlogLists(){
+const data=await fetchQuery(api.posts.getPost);
+return (
+    <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
             {data?.map((post)=>(
                 <Card key={post._id}>
                     <div className="relative h-48 w-full overflow-hidden">
@@ -41,7 +54,22 @@ export default function blogPost(){
                 </Card>
             ))}
         </div>
-       </main>
-      
+)
+}
+
+function SkeletonLoadingUi(){
+    return (
+        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+            {[...Array(6)].map((_,i)=>(
+                <div className="flex flex-col space-y-3" key={i}>
+                    <Skeleton className="h-48 w-full rounded-xl" />
+                    <div className="space-y-2 flex flex-col">
+                        <Skeleton className="h-6 w-3/4" />
+                        <Skeleton className="h-4 w-full" />
+                        <Skeleton className="h-4 w-2/3" />
+                    </div>
+                </div>
+            ))}
+        </div>
     )
 }
