c845a9ca78a5fc9d31e5f4759bf70140f0df1fcd
Haftamu Desta <haftamudesta>
2025-12-20 03:41:24 -0800

    Add createComment function

--- convex/comments.ts
+++ convex/comments.ts
@@ -1,5 +1,6 @@
-import { query } from "./_generated/server";
-import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+import { ConvexError, v } from "convex/values";
+import { authComponent } from "./auth";
 
 export const getCommentsByPostId=query({
     args:{
@@ -8,5 +9,25 @@ export const getCommentsByPostId=query({
     handler:async(ctx,args)=>{
         const data=await ctx.db.query("comments").filter((q)=>q.eq(q.field("postId"),args.postId)).order("desc").collect()
         return data;
+    }
+})
+
+export const createComment=mutation({
+    args:{
+        body:v.string(),
+        postId:v.id("posts")
+    },
+    handler:async(ctx,args)=>{
+        const currentUser=await authComponent.safeGetAuthUser(ctx);
+        if(!currentUser){
+                throw new ConvexError("Not authenticated")
+            }
+        return await ctx.db.insert(("comments"),{
+            postId:args.postId,
+            body:args.body,
+            autherId:currentUser._id,
+            autherName:currentUser.name
+        })
+
     }
 })
