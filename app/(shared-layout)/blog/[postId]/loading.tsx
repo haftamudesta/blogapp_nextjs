e281d7c133af7e521a2fd84f3d2ae9c9d13bfe18
Haftamu Desta <haftamudesta>
2025-12-22 10:39:31 -0800

    Fix some bugs

--- app/(shared-layout)/blog/[postId]/loading.tsx
+++ app/(shared-layout)/blog/[postId]/loading.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function LoadingPage(){
     return (
-        <main className="mac-w-3xl mx-auto py-8 px-4">
+        <main className="max-w-3xl mx-auto py-8 px-4 animate-pulse">
             <Skeleton className="h-10 w-24 mb-6" />
             <Skeleton className="w-full h-[400px] mb-8 rounded-xl" />
             <div className="space-y-4">
